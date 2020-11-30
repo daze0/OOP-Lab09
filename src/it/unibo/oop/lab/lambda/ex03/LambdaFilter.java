f529590a34ca543b3b20bd526886d1526ee2501f
daze <leon.baiocchi@studio.unibo.it>
2020-11-30 18:21:35 +0000

    do ex03 v2

--- src/it/unibo/oop/lab/lambda/ex03/LambdaFilter.java
+++ src/it/unibo/oop/lab/lambda/ex03/LambdaFilter.java
@@ -18,7 +18,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -39,21 +39,31 @@ import java.util.List;
 public final class LambdaFilter extends JFrame {
 
     private static final long serialVersionUID = 1760990730218643730L;
+    private static final String ANY_NON_WORD = "(\\s|\\p{Punct})+";
 
     private enum Command {
         IDENTITY("No modifications", Function.identity()),
         LOWERCASE("Convert to lower case", String::toLowerCase),
-        COUNT_CHARS("Count the number of chars", s -> String.valueOf(s.chars()
-                                                                      .count())),
-        COUNT_LINES("Count the number of lines", s -> String.valueOf(s.chars()
-                                                                      .filter(c -> Character.toString(c).equals("\n"))
-                                                                      .count())),
-        LIST_ALPHA("List all the words in alphabetical order", s -> List.of(s.split("\\s"))
-                                                                        .stream()
-                                                                        .sorted()
-                                                                        .map(String::toString)
-                                                                        .collect(Collectors.joining("\n", "", ""))
-                                                                        .toString());
+        COUNT_CHARS("Count the number of chars", s -> 
+            String.valueOf(s.chars()
+                            .count())),
+        COUNT_LINES("Count the number of lines", s -> {
+            String.valueOf(s.chars()
+                            .filter(c -> Character.toString(c).equals("\n")).count())),
+        LIST_ALPHA("List all the words in alphabetical order", s -> 
+            List.of(s.split(ANY_NON_WORD))
+                     .stream()
+                     .sorted()
+                     .map(String::toString)
+                     .collect(Collectors.joining("\n", "", ""))
+                     .toString()),
+        WORD_COUNT("Count for each word", s -> 
+            Arrays.stream(s.split(ANY_NON_WORD))
+                           .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                           .entrySet().stream()
+                           .map(e -> e.getKey() + " -> " + e.getValue())
+                           .collect(Collectors.joining("\n"))
+        );
 
         private final String commandName; 
         private final Function<String, String> fun;
@@ -61,6 +71,7 @@ public final class LambdaFilter extends JFrame {
         Command(final String name, final Function<String, String> process) {
             commandName = name;
             fun = process; 
+            Arrays.stream(s.split(ANY_NON_WORD)).col;
         }
 
         @Override
